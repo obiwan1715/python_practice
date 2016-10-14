e5f3542e74adc4e871904046ae8ace54cc35f60e
obiwan1715 <benatherton.news@gmail.com>
2016-10-14 14:06:09 +1000

    added new method to convert string case

---
+++ upper_to_lower.py
@@ -0,0 +1,12 @@
+#after much trial and error...
+
+#set method taking 'message' input
+
+def makelower(message):
+	print(message.lower())
+	return
+
+#pass string into method
+
+makelower("HELLO")
+
